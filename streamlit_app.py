07d23ed01bf63f25bb5ae029a951a9a7f9d30986
ssirkn8 <mhdasa10@gmail.com>
2024-01-23 11:21:00 +0700

    changed streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,42 +3,63 @@ import leafmap.foliumap as leafmap
 
 st.set_page_config(layout="wide")
 
-st.sidebar.info(
-    """
-    - Web App URL: <https://streamlit.geemap.org>
-    - GitHub repository: <https://github.com/giswqs/streamlit-geospatial>
-    """
-)
+# st.sidebar.info(
+#     """
+#     - Web App URL: <https://streamlit.geemap.org>
+#     - GitHub repository: <https://github.com/giswqs/streamlit-geospatial>
+#     """
+# )
 
-st.sidebar.title("Contact")
-st.sidebar.info(
-    """
-    Qiusheng Wu: <https://wetlands.io>
-    [GitHub](https://github.com/giswqs) | [Twitter](https://twitter.com/giswqs) | [YouTube](https://www.youtube.com/c/QiushengWu) | [LinkedIn](https://www.linkedin.com/in/qiushengwu)
-    """
-)
+# st.sidebar.title("Contact")
+# st.sidebar.info(
+#     """
+#     Qiusheng Wu: <https://wetlands.io>
+#     [GitHub](https://github.com/giswqs) | [Twitter](https://twitter.com/giswqs) | [YouTube](https://www.youtube.com/c/QiushengWu) | [LinkedIn](https://www.linkedin.com/in/qiushengwu)
+#     """
+# )
 
 # Customize page title
-st.title("Streamlit for Geospatial Applications")
+st.title("WebGIS Applications by Smart System Center (SSC)")
 
 st.markdown(
     """
-    This multipage app template demonstrates various interactive web apps created using [streamlit](https://streamlit.io) and [leafmap](https://leafmap.org). It is an open-source project and you are very welcome to contribute to the [GitHub repository](https://github.com/giswqs/streamlit-multipage-template).
+    This multi-page web app demonstrates various interactive WebGIS apps derived from GIS analysis created using [streamlit](https://streamlit.io) and open-source mapping libraries, 
+    mainly [leafmap](https://leafmap.org) and [geemap](https://geemap.org).
+
+    (Adopted from: [streamlit-multipage-template](https://github.com/giswqs/streamlit-multipage-template) by [Dr. Qiusheng Wu](https://github.com/giswqs))
     """
 )
 
-st.header("Instructions")
+# st.header("Instructions")
 
-markdown = """
-1. For the [GitHub repository](https://github.com/giswqs/streamlit-multipage-template) or [use it as a template](https://github.com/giswqs/streamlit-multipage-template/generate) for your own project.
-2. Customize the sidebar by changing the sidebar text and logo in each Python files.
-3. Find your favorite emoji from https://emojipedia.org.
-4. Add a new app to the `pages/` directory with an emoji in the file name, e.g., `1_🚀_Chart.py`.
+# markdown = """
+# 1. For the [GitHub repository](https://github.com/giswqs/streamlit-multipage-template) or [use it as a template](https://github.com/giswqs/streamlit-multipage-template/generate) for your own project.
+# 2. Customize the sidebar by changing the sidebar text and logo in each Python files.
+# 3. Find your favorite emoji from https://emojipedia.org.
+# 4. Add a new app to the `pages/` directory with an emoji in the file name, e.g., `1_🚀_Chart.py`.
 
+# """
+
+# st.markdown(markdown)
+
+# m = leafmap.Map(minimap_control=True)
+# m.add_basemap("OpenTopoMap")
+# m.to_streamlit(height=500)
+
+st.info("Click on the left sidebar menu to navigate to the different apps.")
+
+st.subheader("Timelapse of Satellite Imagery")
+st.markdown(
 """
+    The following timelapse animations were created using the Timelapse web app. Click `Timelapse` on the left sidebar menu to create your own timelapse for any location around the globe.
+"""
+)
 
-st.markdown(markdown)
+row1_col1, row1_col2 = st.columns(2)
+with row1_col1:
+    st.image("https://github.com/giswqs/data/raw/main/timelapse/spain.gif")
+    st.image("https://github.com/giswqs/data/raw/main/timelapse/las_vegas.gif")
 
-m = leafmap.Map(minimap_control=True)
-m.add_basemap("OpenTopoMap")
-m.to_streamlit(height=500)
+with row1_col2:
+    st.image("https://github.com/giswqs/data/raw/main/timelapse/goes.gif")
+    st.image("https://github.com/giswqs/data/raw/main/timelapse/fire.gif")
